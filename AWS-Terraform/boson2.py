deaebc3967865bbe774f1fd0cc826240a46d2ffb
mdsosnkowski <mdsosnkowski@gmail.com>
2025-08-28 17:08:54 -0400

    a

---
+++ AWS-Terraform/boson2.py
@@ -0,0 +1,13 @@
+pd = {
+    'users' : {},
+    'group' : 'MyUsers',
+    'gid'   : 1001,
+    'osVer' : 10.15,
+    'perms' : [
+        'read',
+        'write',
+        'execute',
+        'print'
+    ]
+}
+print(pd)
