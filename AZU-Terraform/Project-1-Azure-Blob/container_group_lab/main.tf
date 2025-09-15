184bfc50f769446fb3ae4136b19f764f97a5f0ac
mdsosnkowski <mdsosnkowski@gmail.com>
2025-09-15 14:53:12 -0400

    80/20

--- AZU-Terraform/Project-1-Azure-Blob/container_group_lab/main.tf
+++ AZU-Terraform/Project-1-Azure-Blob/container_group_lab/main.tf
@@ -1,54 +1,26 @@
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the resource group provided by the lab."
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-example"
+  location = "East US"
 }
 
-
-variable "storage_account_name" {
-    type      = string
-}
-
-variable "azurerm_storage_account" {
-    type      = string
-}
-
-variable "prefix" {
-  type        = string
-  description = "Prefix to be used for all resources in this lab."
-}
-
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-}
-
-data "azurerm_resource_group" "main" {
-  name = var.resource_group_name
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "sa" {
+  name                     = "examplestoracct"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "example" {
-  name                  = "content"
-  storage_account_name    = azurerm_storage_account.example.id
+resource "azurerm_storage_container" "container" {
+  name                  = "mycontainer"
+  storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
 
-resource "azurerm_storage_blob" "example" {
-  name                   = "my-awesome-content.zip"
-  storage_account_name   = azurerm_storage_account.example.name
-  storage_container_name = azurerm_storage_container.example.name
+resource "azurerm_storage_blob" "blob" {
+  name                   = "myfile.txt"
+  storage_account_name   = azurerm_storage_account.sa.name
+  storage_container_name = azurerm_storage_container.container.name
   type                   = "Block"
-  source                 = "some-local-file.zip"
+  source                 = "localfile.txt"
 }
